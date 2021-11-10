82d078bb2bb66c7cc3253c7e17732ccd3de247d0
Rene Steinmaßl <e1106495@student.tuwien.ac.at>
2021-11-10 07:11:49 +0100

    Add simple Makefile

--- AWS_Test/src/api.py
+++ AWS_Test/src/api.py
@@ -8,4 +8,5 @@ from routes import configure_routes
 configure_routes(api)
 
 if __name__ == '__main__':
+    print("Starting API...")
     serve(api, host=os.environ.get("API_HOST", "0.0.0.0"), port=os.environ.get("API_PORT", "8080"))
