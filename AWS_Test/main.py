81f1573be3b89397379b818110674e1f171c6448
Rene Steinmaßl <e1106495@student.tuwien.ac.at>
2022-02-07 14:01:38 +0100

    revert to flask run instead of waitress

--- AWS_Test/main.py
+++ AWS_Test/main.py
@@ -9,4 +9,4 @@ configure_routes(api)
 
 if __name__ == '__main__':
     print("Starting API...")
-    serve(api, host=os.environ.get("API_HOST", "0.0.0.0"), port=os.environ.get("API_PORT", "8080"))
+    api.run(host="0.0.0.0", port=80, debug=True)
